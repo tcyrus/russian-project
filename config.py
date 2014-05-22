757ace3ba9b8812e822846233b7652cff7ddd940
tcyrus <tim.h.cyrus@gmail.com>
2014-05-22 15:49:41 -0400

    Login

---
+++ config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI="postgresql://flask:flask@localhost/flask"
+DEBUG=True
+SECRET_KEY='RussNet'
--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import os
 from flask import Flask,session,request,render_template,redirect,url_for
-from flask.ext.login import LoginManager,current_user,login_required,login_user,logout_user,UserMixin,confirm_login,fresh_login_required
+from flask.ext.login import LoginManager,current_user,login_required,login_user,logout_user,UserMixin
+from flask.ext.sqlalchemy import SQLAlchemy
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+tmpl_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
 
-app = Flask(__name__,template_folder=tmpl_dir)
-app.config.from_object(__name__)
-
-app.config.update(dict(
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-
-app.config.from_envvar('MAIN_SETTINGS',silent=True)
+app=Flask(__name__,template_folder=tmpl_dir)
+app.config.from_pyfile("config.py")
+db=SQLAlchemy(app)
 login_manager=LoginManager()
 login_manager.init_app(app)
 
+# User class
+class DbUser(object):
+    """Wraps User object for Flask-Login"""
+    def __init__(self, user):
+        self._user=user
+    def get_id(self):
+        return unicode(self._user.id)
+    def is_active(self):
+        return self._user.enabled
+    def is_anonymous(self):
+        return False
+    def is_authenticated(self):
+        return True
+
 @login_manager.user_loader
-def load_user(userid):
-    return User.get(userid)
+def load_user(user_id):
+    user=User.query.get(user_id)
+    if user:
+        return DbUser(user)
+    else:
+        return None
 
 @app.route('/',methods=['GET','POST'])
 def home():
@@ -28,13 +39,20 @@ def home():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+  error=None
   if request.method=='POST':
-    if request.form.validate_on_submit():
-        login_user(user)
-        flash("Logged in successfully.")
-        return redirect(url_for("dashboard"))
+      username=request.form['username']
+      password=request.form['password']
+      if authenticate(app.config['AUTH_SERVER'],username,password):
+        user=User.query.filter_by(username=username).first()
+        if user:
+          if login_user(DbUser(user)):
+            # do stuff
+            flash("You have logged in")
+            return redirect(url_for("dashboard"))
+      error="Login failed"
   else:
-    return render_template("login.html")
+    return render_template("login.html",error=error)
 
 @app.route("/settings")
 @login_required
@@ -45,6 +63,7 @@ def settings():
 @login_required
 def logout():
     logout_user()
+    flash('You have logged out')
     return redirect(url_for("home"))
 
 @app.route('/dashboard',methods=['GET','POST'])
@@ -52,7 +71,7 @@ def dashboard():
     """
     Dashboard
     """
-    # ToDo
+    return render_template("dashboard.html")
 
 if __name__=='__main__':
     app.run()
